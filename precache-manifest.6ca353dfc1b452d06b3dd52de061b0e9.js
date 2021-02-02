9b05c8d6992458fe1aa4d9d987ff571e9a2d54df
295bsumeet <bsumeet295@gmail.com>
2021-02-02 03:23:13 -0500

    Updates

---
+++ precache-manifest.6ca353dfc1b452d06b3dd52de061b0e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6159038f4d287f25703b9e40fa806e20",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "67be8f1ca13ab1177c31",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67be8f1ca13ab1177c31",
+    "url": "/home/static/js/main.0a6cb0a9.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "24770533bcad8ce61b5b3329f11f9445",
+    "url": "/home/static/media/Resume_Sumeet_Bhadale.24770533.pdf"
+  },
+  {
+    "revision": "0c862a459a652406a791c00aa442c3df",
+    "url": "/home/static/media/Sumeet.0c862a45.png"
+  }
+]);
